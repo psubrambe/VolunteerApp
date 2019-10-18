e1dc0485dec24e9c061e18cb8c115df5c602faf7
mpmansfield <matthewpmansfield@gmail.com>
2019-10-18 16:20:53 -0400

    Event class for creating event objects to be written to firebase. Doesn't currently include OrgId. Date/Time may need adjustment.

---
+++ app/src/main/java/com/mrpanda2/volunteerapp/Event.java
@@ -0,0 +1,37 @@
+package com.mrpanda2.volunteerapp;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class Event {
+
+    private UUID mId;
+    //TODO: add specific orgID as foreign key
+    private String mName;
+    private Date mDate;
+    private String mTime;
+    private String mLocation;
+
+    public Event(){
+        mId = UUID.randomUUID();
+    }
+
+    public UUID getId() { return mId; }
+
+    public String getName(){ return mName;}
+
+    public void setName(String name){ mName = name; }
+
+    public Date getDate(){return mDate;}
+
+    public void setDate(Date date){mDate = date;}
+
+    public String getTime(){return mTime;}
+
+    public void setTime(String time){mTime = time;}
+
+    public String getLocation(){return mLocation;}
+
+    public void setLocation(String location){mLocation = location;}
+
+}
