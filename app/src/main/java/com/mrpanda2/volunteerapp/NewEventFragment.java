e387a914dc20cd219240183fc5950c259d663fbd
mpmansfield <matthewpmansfield@gmail.com>
2019-10-29 00:13:43 -0400

    Added Date/Time Pickers to New Event Fragment.

--- app/src/main/java/com/mrpanda2/volunteerapp/NewEventFragment.java
+++ app/src/main/java/com/mrpanda2/volunteerapp/NewEventFragment.java
@@ -1,15 +1,21 @@
 package com.mrpanda2.volunteerapp;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
@@ -19,11 +25,16 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.Calendar;
+
 public class NewEventFragment extends Fragment {
     private Event mEvent;
     private EditText mNameField;
-    private EditText mDateField;
-    private EditText mTimeField;
+    //private EditText mDateField;
+    private TextView mDateField;
+    private Button mDateButton;
+    private TextView mTimeField;
+    private Button mTimeButton;
     private EditText mLocationField;
     private Button mCreateButton;
     private FirebaseUser mUser;
@@ -61,7 +72,7 @@ public class NewEventFragment extends Fragment {
         });
 
         //DATE
-        mDateField = (EditText) v.findViewById(R.id.new_event_date);
+        /*mDateField = (EditText) v.findViewById(R.id.new_event_date);
         mDateField.addTextChangedListener(new TextWatcher(){
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after){
@@ -75,10 +86,28 @@ public class NewEventFragment extends Fragment {
             public void afterTextChanged(Editable s){
                 //blank
             }
+        });*/
+
+        mDateField = v.findViewById(R.id.new_event_date);
+        mDateButton = v.findViewById(R.id.new_event_date_button);
+        mDateButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                handleDate();
+            }
+        });
+        mTimeField = v.findViewById(R.id.new_event_time);
+        mTimeButton = v.findViewById(R.id.new_event_time_button);
+        mTimeButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                handleTime();
+            }
         });
 
+
         //Event Time
-        mTimeField = (EditText) v.findViewById(R.id.new_event_time);
+        /*mTimeField = (EditText) v.findViewById(R.id.new_event_time);
         mTimeField.addTextChangedListener(new TextWatcher(){
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after){
@@ -92,7 +121,7 @@ public class NewEventFragment extends Fragment {
             public void afterTextChanged(Editable s){
                 //blank
             }
-        });
+        });*/
 
         //Event Location
         mLocationField = (EditText) v.findViewById(R.id.new_event_location);
@@ -115,18 +144,75 @@ public class NewEventFragment extends Fragment {
         mCreateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v){
-                Log.d("Tag", "Database write attempt.");
-                mDatabase = FirebaseDatabase.getInstance().getReference();
-                mDatabase.child("events").child(mEvent.getId().toString()).setValue(mEvent);
-                Toast.makeText(NewEventFragment.this.getActivity(), "Event Created", Toast.LENGTH_SHORT).show();
+
+                if (mEvent.getDate() == null || mEvent.getName() == null || mEvent.getLocation() == null || mEvent.getTime() == null){
+                    Toast.makeText(NewEventFragment.this.getActivity(), "Field is empty.", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Log.d("Tag", "Database write attempt.");
+                    mDatabase = FirebaseDatabase.getInstance().getReference();
+                    mDatabase.child("events").child(mEvent.getId().toString()).setValue(mEvent);
+                    Toast.makeText(NewEventFragment.this.getActivity(), "Event Created", Toast.LENGTH_SHORT).show();
                 /*Intent intent = new Intent(NewEventFragment.this.getActivity(), organizationSignInPage.class);
                 startActivity(intent);*/
-                Intent intent = EventActivity.newIntent(NewEventFragment.this.getActivity(), mEvent.getId());
-                startActivity(intent);
+                    Intent intent = EventActivity.newIntent(NewEventFragment.this.getActivity(), mEvent.getId());
+                    startActivity(intent);
+                }
             }
         });
 
         return v;
     }
+
+    private void handleDate() {
+        Calendar cal = Calendar.getInstance();
+        int YEAR = cal.get(Calendar.YEAR);
+        int MONTH = cal.get(Calendar.MONTH);
+        int DATE = cal.get(Calendar.DATE);
+
+        DatePickerDialog datePicker = new DatePickerDialog(NewEventFragment.this.getActivity(), new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
+
+                Calendar calFrag = Calendar.getInstance();
+                calFrag.set(Calendar.YEAR, year);
+                calFrag.set(Calendar.MONTH, month);
+                calFrag.set(Calendar.DATE, dayOfMonth);
+                month = month + 1;
+                String dateText = month + "/" + dayOfMonth + "/" + year;
+
+                mDateField.setText(dateText);
+                mEvent.setDate(dateText);
+            }
+        }, YEAR, MONTH, DATE);
+
+        datePicker.show();
+    }
+
+    private void handleTime(){
+        Calendar cal = Calendar.getInstance();
+        int HOUR = cal.get(Calendar.HOUR);
+        int MINUTE = cal.get(Calendar.MINUTE);
+        boolean is24Hour = DateFormat.is24HourFormat(NewEventFragment.this.getActivity());
+
+        TimePickerDialog timePicker = new TimePickerDialog(NewEventFragment.this.getActivity(), new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hour, int minute) {
+                Calendar calFrag = Calendar.getInstance();
+                calFrag.set(Calendar.HOUR, hour);
+                calFrag.set(Calendar.MINUTE, minute);
+                String timeText = DateFormat.format("h:mm a", calFrag).toString();
+
+                mTimeField.setText(timeText);
+                mEvent.setTime(timeText);
+            }
+        }, HOUR, MINUTE, is24Hour);
+
+
+        timePicker.show();
+
+    }
+
+
 
 }
